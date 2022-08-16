92fe1ce3f3c29480492a3c6fc40366be2ad257e2
yairgyee <51312790+yairgyee@users.noreply.github.com>
2022-08-16 14:49:09 +0630

    update

--- controllers/Report.js
+++ controllers/Report.js
@@ -1,85 +1,42 @@
 const ErrorResponse = require("../utils/ErrorResponse");
 const asyncHandler = require("../middlewares/async");
 const colors = require("colors");
-const AgentsReport = require("../models/Reports/AgentsReport");
-const User = require("../models/User");
-const Call = require("../models/Call");
-const Report = require("../models/Report");
-// const Report = require("../models/Report");
+const Lager = require("../models/Lager");
 
-// Desc    GET USERS
-// Route   GET api/v1/reports/calls
-exports.agentReports = asyncHandler(async (req, res, next) => {
-  let { agentId, lotteryId } = req.params;
-  console.log(colors.bgRed(req.params.agentId));
+exports.allTotal = asyncHandler(async (req, res, next) => {
+  console.log(req.originalUrl);
+  let report;
 
-  const report = await Report.find({
-    userId: agentId ? agentId : req.user._id,
-  }).populate({
-    path: "data.calls",
-    select: "callname totalAmount win status",
-    path: "userId",
-    select: "username role",
-  });
-
-  const resReport = report.filter(
-    (rp) => rp.lottery.toString() === lotteryId.toString()
-  );
-
-  console.log(req.user);
-  res.status(200).json({ success: true, resReport });
-});
-
-// Desc GET Masters
-// Route GET api/v1/reports/master
-exports.masterReports = asyncHandler(async (req, res, next) => {
-  console.log(colors.bgRed(req.user));
-  const { lotteryId } = req.params;
-  const report = await Report.find({ userId: req.user._id }).populate({
-    path: "data.users",
-    select: "username role",
-    path: "userId",
-    select: "username role",
-  });
-  const resReport = report.filter(
-    (rp) => rp.lottery.toString() === lotteryId.toString()
-  );
-  console.log(resReport);
-  res.status(200).json({ success: true, data: "Masters-Report", resReport });
-});
-
-// Desc GET agents of one master
-// Route GET api/v1/reports/master/agents
-exports.getMaster_select_agents = asyncHandler(async (req, res, next) => {
-  const { lotteryId } = req.params;
-  const report = await Report.find({ createByUser: req.user._id }).populate({
-    path: "data.users",
-    select: "username role",
-    path: "userId",
-    select: "username role",
-  });
-
-  const resReport = report.filter(
-    (rp) => rp.lottery.toString() === lotteryId.toString()
-  );
-  console.log(resReport);
-  res.status(200).json({ success: true, resReport });
-});
-
-// Desc GET agent of one master
-// Route GET api/v1/reports/master/agents/:agentId
-exports.getMaster_select_agents_select_agentId = asyncHandler(
-  async (req, res, next) => {
-    res.status(200).json({ success: true, message: req.originalUrl });
+  const lager = await Lager.find({ user: req.user._id })
+    .populate({
+      path: "user",
+      select: "username name",
+    })
+    .populate({
+      path: "createByUser",
+      select: "username role",
+    });
+  if (!lager) {
+    return next(new ErrorResponse("Lager not found", 404));
   }
-);
 
-exports.createAgent = asyncHandler(async (req, res, next) => {
-  console.log(colors.bgGreen(req.body));
-});
+  // const IN = await lager.in;
 
-// Desc GET Admin
-// Route GET api/v1/reports/admin
-exports.getAdmins = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, data: "Agents-Report" });
+  // console.log(lager.in.totalAmount);
+
+  report = {
+    me: {
+      totalAmount: lager.in,
+      win: lager.in,
+    },
+  };
+
+  console.log(report);
+
+  res.status(200).json({
+    success: true,
+    // data: IN,
+    report,
+    url: req.originalUrl,
+  });
 });
--- routes/Report.js
+++ routes/Report.js
@@ -1,11 +1,6 @@
 const express = require("express");
 
-const {
-  masterReports,
-  getAdmins,
-  agentReports,
-  getMaster_select_agents,
-} = require("../controllers/Report");
+const { allTotal } = require("../controllers/Report");
 const callRouter = require("./Call");
 const agentRouter = require("./Agent");
 
@@ -13,17 +8,6 @@ const { protect, authorize } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.use("/agent/calls", protect, callRouter);
-router.route("/master/agents/:lotteryId").get(protect, getMaster_select_agents);
-router.route("/master/agents/:agentId/:lotteryId").get(protect, agentReports);
-router.use("/master/agents/:agentId/calls", protect, callRouter);
-
-// router.route("/calls").get(getCalls);
-router.route("/agent/:lotteryId").get(protect, agentReports);
-router.use("/agent/:agentId/calls", protect, callRouter);
-router
-  .route("/master/:lotteryId")
-  .get(protect, authorize("Master"), masterReports);
-router.route("/admin").get(getAdmins);
+router.route("/all-total").get(protect, allTotal);
 
 module.exports = router;
